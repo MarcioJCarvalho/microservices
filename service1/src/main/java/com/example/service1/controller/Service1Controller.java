7cfc1e56c067f13cdc2698f6c17a1afdea377b81
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2023-11-20 20:59:54 -0300

    implementação dos microservices

---
+++ service1/src/main/java/com/example/service1/controller/Service1Controller.java
@@ -0,0 +1,14 @@
+package com.example.service1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service1")
+public class Service1Controller {
+    @GetMapping
+    public String getservice(){
+        return "Olá eu sou o service1";
+    }
+}
